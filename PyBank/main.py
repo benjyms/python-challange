0007df36dd53389a45447f60e52e4c5b5cfa37c6
benjyms <87244593+benjyms@users.noreply.github.com>
2021-08-19 17:38:44 -0400

    open import and read .csv complete and debugged

---
+++ PyBank/main.py
@@ -0,0 +1,21 @@
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV files
+import csv
+
+# Read and store the data from the .csv file
+data = open(r"C:\Users\benjy\Documents\Bootcamp Repos\python-challange\PyBank\Resources\budget_data.csv")
+data = csv.reader(data)  
+print(data)
+
+data_header = next(data)
+
+# print(data_header)
+print(f"CSV Header: {data_header}")
+
+# # Read each row of data after the header
+for row in data:
+    print(row)
+
